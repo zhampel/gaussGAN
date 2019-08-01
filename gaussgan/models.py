4718c2d35cccc3a48a1c9b0303ed4fee3f38afeb
zhampel <17304045+zhampel@users.noreply.github.com>
2019-08-01 11:50:29 -0700

    Some commented out activation functions for testing

--- gaussgan/models.py
+++ gaussgan/models.py
@@ -17,6 +17,15 @@ except ImportError as e:
     print(e)
     raise ImportError
 
+import math
+class GELU(nn.Module):
+    """
+    Paper Section 3.4, last paragraph notice that BERT used the GELU instead of RELU
+    """
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
 
 class Reshape(nn.Module):
     """
@@ -56,26 +65,35 @@ class Generator(nn.Module):
         self.model = nn.Sequential(
             # Fully connected layers
             torch.nn.Linear(self.latent_dim, self.scaled_x_lat),
-            torch.nn.Dropout(0.2),
-            #nn.BatchNorm1d(self.scaled_x_lat),
+            #torch.nn.Dropout(0.2),
+            nn.BatchNorm1d(self.scaled_x_lat),
             ##torch.nn.Linear(self.latent_dim, 512),
             ##nn.BatchNorm1d(512),
-            ##torch.nn.ReLU(True),
             nn.LeakyReLU(0.2, inplace=True),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.Sigmoid(),
+            #nn.ReLU(),
 
             #torch.nn.Linear(self.scaled_x_lat, 512),
             #nn.BatchNorm1d(512),
             #nn.LeakyReLU(0.2, inplace=True),
             torch.nn.Linear(self.scaled_x_lat, self.scaled_x_lat),
-            torch.nn.Dropout(0.2),
-            #nn.BatchNorm1d(self.scaled_x_lat),
+            #torch.nn.Dropout(0.2),
+            nn.BatchNorm1d(self.scaled_x_lat),
             nn.LeakyReLU(0.2, inplace=True),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.ReLU(),
             
             #torch.nn.Linear(512, self.scaled_x_dim),
             torch.nn.Linear(self.scaled_x_lat, self.scaled_x_dim),
-            torch.nn.Dropout(0.2),
-            #nn.BatchNorm1d(self.scaled_x_dim),
+            #torch.nn.Dropout(0.2),
+            nn.BatchNorm1d(self.scaled_x_dim),
             nn.LeakyReLU(0.2, inplace=True),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.ReLU(),
             
             torch.nn.Linear(self.scaled_x_dim, x_dim),
         )
@@ -112,10 +130,16 @@ class Discriminator(nn.Module):
         self.model = nn.Sequential(
             nn.Linear(self.dim, self.scaled_x_dim),
             nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Dropout(0.2),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.ReLU(),
+            #torch.nn.Dropout(0.2),
             nn.Linear(self.scaled_x_dim, 512),
             nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Dropout(0.2),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.ReLU(),
+            #torch.nn.Dropout(0.2),
             #nn.Linear(self.scaled_x_dim, 1024),
             #nn.LeakyReLU(0.2, inplace=True),
             #nn.Linear(1024, 512),
@@ -124,12 +148,18 @@ class Discriminator(nn.Module):
             # Fully connected layers
             torch.nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Dropout(0.2),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.ReLU(),
+            #torch.nn.Dropout(0.2),
             
             # Fully connected layers
             torch.nn.Linear(256, 128),
             nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Dropout(0.2),
+            #nn.Sigmoid(),
+            #GELU(),
+            #nn.ReLU(),
+            #torch.nn.Dropout(0.2),
             torch.nn.Linear(128, 1),
         )
         
